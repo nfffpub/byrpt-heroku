63e9a6fcdbfdba2bdfc2c9df683b9587f5ff4d13
nfffpub <91594010+nfffpub@users.noreply.github.com>
2021-12-14 10:45:09 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+  "name": "Try NJS",
+  "description": "A Test program from NodeJS",
+  "repository": "https://github.com/nfffpub/nhentai-heroku",
+  "keywords": ["node", "express", "heroku"],
+  "image": "heroku/heroku"
+}
